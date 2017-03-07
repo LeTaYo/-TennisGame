aeb55aec21f6f3ac4e2c7191178c89e2c792e709
younes <younes@192.168.1.34>
2017-03-07 21:21:54 +0100

    feat(service): add getter ans setter

--- EnedisTenisGame/src/main/java/Player.java
+++ EnedisTenisGame/src/main/java/Player.java
@@ -1,12 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
 
 public class Player {
-	
+	public static final List<String> POINTSNAME = Arrays.asList("love", "fifteen", "thirty", "forty");
 	private int score;
 	private String namePlayer;
 	
 	public Player(String namePlayer) {       
 	 this.namePlayer = namePlayer;
 	}
-
-	 
+	public int getScore() {
+	     return score;    
+	}  
+	public String getName() {	      
+		  return namePlayer;
+	}
+	public void winPoint() {	       
+		 this.score = this.score + 1;
+	} 
 }
